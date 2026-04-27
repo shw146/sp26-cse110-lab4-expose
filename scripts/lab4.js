8b86ae4a98639a1db12bc0b6aceda9b7e87b06b8
shw146 <shw146@ucsd.edu>
2026-04-27 08:18:58 -0700

    fixed missed edge cases of js file

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add) {
+    if (add && typeof num1 == number && typeof num2 == number) {
         let result = 0;
 
         result = num1 + num2;
@@ -14,7 +14,7 @@ function sumValues(num1, num2, add) {
         return result;
     }
     else {
-        return add;
+        return false;
     }
 }
 
@@ -25,6 +25,11 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+
+    if(!Array.isArray(prices)){
+        return false;
+    }
+
     const discounted = [];
     const length = prices.length;
 
@@ -34,7 +39,7 @@ function discountPrices(prices, discount) {
 
     let discountedPrice = 0;
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
