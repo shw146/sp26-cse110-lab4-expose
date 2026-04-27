f3e3f3b88c83e4c8262b26b6e15fa193f354aea0
shw146 <shw146@ucsd.edu>
2026-04-27 08:21:53 -0700

    number string fixed

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -6,7 +6,7 @@
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
 function sumValues(num1, num2, add) {
-    if (add && typeof num1 == number && typeof num2 == number) {
+    if (add && typeof num1 == "number" && typeof num2 == "number") {
         let result = 0;
 
         result = num1 + num2;
